4c639048f8103238f2ff7b709990a6aa75857fef
Ayamit <info@azalea-a.com>
2024-03-26 17:55:13 +0900

    20240326_18:00

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,46 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+      //ハンバーガー
+    $(function () {
+      // ハンバーガーメニュー
+      $(".js-hamburger,.js-drawer a").click(function () {
+        $(".js-hamburger").toggleClass("is-active");
+        $(".js-drawer").fadeToggle();
+      });
+    });
+
+    //fvスライダー
+    const swiper1 = new Swiper(".js-fvSwiper", {
+      loop: true,
+      effect: "slide",
+      speed: 4000,
+      allowTouchMove: false,
+      autoplay: {
+        delay: 3000,
+      },
+    });
+
+    //topページ campaignのswiper
+    const swiper2 = new Swiper(".js-topCampaignSwiper", {
+      slidesPerView: "auto",
+      loop: "true",
+      speed: 3000,
+      spaceBetween: 24,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      breakpoints: {
+        768: {
+          spaceBetween: 40,
+        },
+      }
+
+    });
+
 
 });
