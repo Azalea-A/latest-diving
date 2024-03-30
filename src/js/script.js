7abc216015d520191f6fb9bdc601428b86477a94
Ayamit <info@azalea-a.com>
2024-03-30 20:10:59 +0900

    20240330_20:00_toTopまで

--- src/js/script.js
+++ src/js/script.js
@@ -46,5 +46,39 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
     });
 
+  //topへ戻る
+    //トップへ戻るボタン
+$(function () {
+  const pagetop = $("#js-pagetop");
+  pagetop.hide(); //最初はボタンを非表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) { //100px以上スクロールしたら
+      pagetop.fadeIn(); //ボタンをフェードイン
+    } else {
+      pagetop.fadeOut(); //ボタンをフェードアウト
+    }
+  });
+  pagetop.click(function () {
+    $("body,html").animate({
+        scrollTop: 0, //上から0pxの位置に戻る
+      },800); //800ミリ秒かけて上に戻る
+    return false;
+  });
+});
 
+// スクロール時にフッター手前で止まるボタンの表示位置を調整する
+  $(document).ready(function () {
+    $(".js-toTop").hide();
+    $(window).on("scroll", function () {
+      let scrollHeight = $(document).height(); // ドキュメントの高さ
+      let scrollPosition = $(window).height() + $(window).scrollTop(); // 現在の位置
+      let footHeight = $(".footer").innerHeight(); // フッターの高さ
+      if (scrollHeight - scrollPosition <= footHeight) {
+        $(".js-toTop").css({ position: "absolute", bottom: footHeight}); // ボタンをフッター手前に固定
+      } else {
+        $(".js-toTop").css({ position: "fixed", bottom: 20 }); // ボタンを固定表示
+      }
+    });
+    $(".js-toTop").show(); // ボタンを表示する
+  });
 });
