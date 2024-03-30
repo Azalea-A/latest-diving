cc88f92cddf4b534336d761a39da00fb5671a846
Ayamit <info@azalea-a.com>
2024-03-30 22:30:03 +0900

    20240330_inviewsまで22:30

--- src/js/script.js
+++ src/js/script.js
@@ -81,4 +81,34 @@ $(function () {
     });
     $(".js-toTop").show(); // ボタンを表示する
   });
+
+
+//information, voice, priceの画像のエフェクト
+
+var box = $('.color-box'),
+speed = 700;
+//↑要素の取得とスピードの設定
+//.color-boxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+$(this).append('<div class="js-color-box"></div>')
+var color = $(this).find($('.js-color-box')),
+image = $(this).find('img');
+var counter = 0;
+
+image.css('opacity','0');
+color.css('width','0%');
+//inviewを使って背景色が画面に現れたら処理をする
+$(this).on('inview', function(){
+    if(counter == 0){
+      $(color).delay(200).animate({'width':'100%'}, speed, function(){
+        $(image).css('opacity','1');
+        $(color).css({'left':'0', 'right':'auto'});
+        $(color).animate({'width':'0'}, speed);
+      });
+      counter = 1;
+    }
+ });
+});
+
+
 });
