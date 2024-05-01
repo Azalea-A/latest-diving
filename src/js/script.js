a98ed6579ccd3a0494107e93dd491034b4371271
Ayamit <info@azalea-a.com>
2024-05-01 19:36:10 +0900

    pagenaviうまく行った！

--- src/js/script.js
+++ src/js/script.js
@@ -30,7 +30,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     //topページ campaignのswiper
     const swiper2 = new Swiper(".js-topCampaignSwiper", {
       slidesPerView: "auto",
-      loop: "true",
+      loop: true,
       speed: 3000,
       spaceBetween: 24,
       pagination: {
@@ -87,44 +87,71 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       });
     });
 
-//information, voice, priceの画像のエフェクト
-var box = $('.color-box'),
-speed = 700;
-//↑要素の取得とスピードの設定
-//.color-boxの付いた全ての要素に対して下記の処理を行う
-box.each(function(){
-$(this).append('<div class="js-color-box"></div>')
-var color = $(this).find($('.js-color-box')),
-image = $(this).find('img');
-var counter = 0;
+  //information, voice, priceの画像のエフェクト
+  var box = $('.color-box'),
+  speed = 700;
+  //↑要素の取得とスピードの設定
+  //.color-boxの付いた全ての要素に対して下記の処理を行う
+  box.each(function(){
+  $(this).append('<div class="js-color-box"></div>')
+  var color = $(this).find($('.js-color-box')),
+  image = $(this).find('img');
+  var counter = 0;
 
-image.css('opacity','0');
-color.css('width','0%');
-//inviewを使って背景色が画面に現れたら処理をする
-$(this).on('inview', function(){
-    if(counter == 0){
-      $(color).delay(200).animate({'width':'100%'}, speed, function(){
-        $(image).css('opacity','1');
-        $(color).css({'left':'0', 'right':'auto'});
-        $(color).animate({'width':'0'}, speed);
-      });
-      counter = 1;
-    }
+  image.css('opacity','0');
+  color.css('width','0%');
+  //inviewを使って背景色が画面に現れたら処理をする
+  $(this).on('inview', function(){
+      if(counter == 0){
+        $(color).delay(200).animate({'width':'100%'}, speed, function(){
+          $(image).css('opacity','1');
+          $(color).css({'left':'0', 'right':'auto'});
+          $(color).animate({'width':'0'}, speed);
+        });
+        counter = 1;
+      }
+    });
+  });
+
+//ここから下層ページ
+//下層campaign page タブ
+$(function () {
+  $(".js-category-tab-container.is-active").css("display", "block");
+  $(".js-category-tab").on("click", function () {
+    $(".js-category-tab.current").removeClass("current");
+    $(this).addClass("current");
+    const index = $(this).index();
+    $("js-category-tab-container").hide().eq(index).fadeIn(300);
   });
 });
 
 //下層information page タブ
-$(function () {
-  const informationTabButton = $(".js-information-tab-button"),
-    informationTabContent = $(".js-information-tab-content");
-    informationTabButton.on("click", function () {
-    let index = informationTabButton.index(this);
+  $(function () {
+    const informationTabButton = $(".js-information-tab-button"),
+      informationTabContent = $(".js-information-tab-content");
+      informationTabButton.on("click", function () {
+      let index = informationTabButton.index(this);
 
-    informationTabButton.removeClass("is-active");
-    $(this).addClass("is-active");
-    informationTabContent.removeClass("is-active");
-    informationTabContent.eq(index).addClass("is-active");
+      informationTabButton.removeClass("is-active");
+      $(this).addClass("is-active");
+      informationTabContent.removeClass("is-active");
+      informationTabContent.eq(index).addClass("is-active");
+    });
   });
-});
+
+  //FAQアコーディオン
+  $(function () {
+    $(".js-accordion__item .js-accordion__content").css(
+      "display",
+      "block"
+    );
+    $(".js-accordion__item .js-accordion__title").addClass(
+      "is-open"
+    );
+    $(".js-accordion__title").on("click", function () {
+      $(this).toggleClass("is-open");
+      $(this).next().slideToggle(300);
+    });
+  });
 
 });
